87f09f2929b521c6bd4cd7e1b4577480c53baa96
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-15 13:13:28 +0530

    initial commit with project creation

---
+++ src/test/java/com/boot/thymleafPractical/ThymeleafPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.thymleafPractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
